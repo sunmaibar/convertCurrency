b05c7aebc8be3dbaeb7a8c5b940dd856b7b35cb2
Jimmy <jajalaba@yahoo.com.tw>
2020-02-17 21:57:53 +0800

    Hello

---
+++ precache-manifest.9d0a00f0263530c992bcb7b522b32be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503c86888b9e6cc49ceef50523d7232e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2649105dae46d8b706ad",
+    "url": "/static/css/main.55bf56ab.chunk.css"
+  },
+  {
+    "revision": "f6c1cbd606c64ea76b5c",
+    "url": "/static/js/2.63ce82f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.63ce82f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2649105dae46d8b706ad",
+    "url": "/static/js/main.9dc870a1.chunk.js"
+  },
+  {
+    "revision": "01b87d12ab7f8c03844c",
+    "url": "/static/js/runtime-main.ff58a2c2.js"
+  }
+]);
